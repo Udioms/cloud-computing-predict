3ec2ec1d099d4a7beaee19ba8b1a3aa9fd7765a3
Udioms <125586662+Udioms@users.noreply.github.com>
2023-05-06 20:50:27 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://5s8ylsgdb2.execute-api.eu-west-1.amazonaws.com/default/Myfunction"
+                url: "https://5s8ylsgdb2.execute-api.eu-west-1.amazonaws.com/default/Myfunction",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
